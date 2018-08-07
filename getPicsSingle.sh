6270fddea30fd8ebbd0fbfedcdaac58eddb7796a
unknown <navya_rawat@yahoo.com>
2018-08-07 22:48:55 +0530

    Silenced wgets and added getPicsMulti

--- getPicsSingle.sh
+++ getPicsSingle.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-picDir=~/Documents/desktop-wallpapers
+picDir=~/Documents/desktop-wallpapers-single
 mkdir $picDir
 rm $picDir/*.jpg $picDir/*.html $picDir/*.txt
 user=$(whoami)
 cd $picDir
-wget http://apod.nasa.gov/apod/astropix.html
+wget -q http://apod.nasa.gov/apod/astropix.html
 a='http://apod.nasa.gov/apod/'
 grep 'href="i' astropix.html > temp.txt
 b=$(awk -F "\"" '{print $2}' temp.txt) 
 c=$a$b
-wget $c
+wget -q $c
 imname=$(ls *.jpg)
 echo $imname
 /users/btech/navyar/bin/gsettings set org.gnome.desktop.background picture-uri file:///$(pwd)/$imname
