7dbfda99bb1950ef784d6cd526cf3c27bfec85c3
unknown <navya_rawat@yahoo.com>
2018-08-07 22:27:13 +0530

    Basic wallpaper changer using image from static NASA website with only a single image

--- getpics.sh
+++ getpics.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-rm ~/apod/*.jpg ~/apod/*.html ~/apod/*.txt
+picDir=~/Documents/desktop-wallpapers
+mkdir $picDir
+rm $picDir/*.jpg $picDir/*.html $picDir/*.txt
 user=$(whoami)
-cd ~/apod
+cd $picDir
 wget http://apod.nasa.gov/apod/astropix.html
 a='http://apod.nasa.gov/apod/'
 grep 'href="i' astropix.html > temp.txt
@@ -10,5 +12,6 @@ b=$(awk -F "\"" '{print $2}' temp.txt)
 c=$a$b
 wget $c
 imname=$(ls *.jpg)
-/usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///$(pwd)/$imname
+echo $imname
+/users/btech/navyar/bin/gsettings set org.gnome.desktop.background picture-uri file:///$(pwd)/$imname
 cd ~/Desktop
